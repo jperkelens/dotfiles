e1889be640176798ac7cfb6943ba5da9d56115b9
Matt Roman <romanmt@gmail.com>
2014-04-14 09:30:23 -0400

    Adds autocomplete config and additional org mode settings

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:dependencies [[org.clojars.gjahad/debug-repl "0.3.3"]
                        [im.chit/vinyasa "0.1.8"]]
+        :plugins [[lein-midje "3.0.0"]]
         :injections [(require 'vinyasa.inject)
                      (vinyasa.inject/inject 'clojure.core '>
                                             '[[clojure.repl doc source]
