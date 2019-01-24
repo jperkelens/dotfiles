c11f54d6943bf23eeea9ab0808ad18d4330b83a2
Matt Roman <romanmt@gmail.com>
2019-01-24 12:16:42 -0500

    move temp files for emacs

--- spacemacs_layer/romanmt/config.el
+++ spacemacs_layer/romanmt/config.el
@@ -7,8 +7,13 @@
 ;; configure neo theme to use icons
 (setq neo-theme 'nerd)
 
-(setq initial-frame-alist '((top . 30) (left . 30) (width . 180) (height . 60)))
+(setq initial-frame-alist '((top . 30) (left . 30) (width . 180) (height . 20)))
 
 (set-face-attribute 'default nil :font "Input Mono" )
-(set-face-attribute 'default nil :height 160 )
+(set-face-attribute 'default nil :height 150 )
 
+;; Move autosaves out of source tree
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
