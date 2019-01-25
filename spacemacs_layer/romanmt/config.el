a3707fc0d1bc14f5357db6ea862ec882c1a52815
Matt Roman <romanmt@gmail.com>
2019-01-25 16:59:57 -0500

    Org mode and other stuff

--- spacemacs_layer/romanmt/config.el
+++ spacemacs_layer/romanmt/config.el
@@ -7,13 +7,19 @@
 ;; configure neo theme to use icons
 (setq neo-theme 'nerd)
 
-(setq initial-frame-alist '((top . 30) (left . 30) (width . 180) (height . 20)))
+(setq initial-frame-alist '((top . 30) (left . 30) (width . 180) (height . 50)))
 
 (set-face-attribute 'default nil :font "Input Mono" )
-(set-face-attribute 'default nil :height 150 )
+(set-face-attribute 'default nil :height 160 )
 
 ;; Move autosaves out of source tree
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;;;;Org mode configuration
+;; Enable Org mode
+(require 'org)
+;; Make Org mode work with files ending in .org
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
